87d0be798d7393060291e850186f2d9873db56cc
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-10-09 10:36:21 +0530

    litle change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import Blog from "components/Blog";
+import Contact from "components/Contact";
+import Footer from "components/Footer";
+import Home from "components/Home";
+import Milestones from "components/Milestones";
+import Portfolio from "components/Portfolio";
+import Pricing from "components/Pricing";
+import ScrollToTop from "components/ScrollToTop";
+import Services from "components/Services";
+import Skills from "components/Skills";
+import Testimonials from "components/Testimonials";
+import Video from "components/Video";
+import React from "react";
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <ScrollToTop />
+      <Home />
+      <Services />
+      <Portfolio />
+      <Milestones />
+      <Blog />
+      <Video />
+      <Pricing />
+      <Testimonials />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 }
