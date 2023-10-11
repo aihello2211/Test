d5211cf9b4a503fed02f397685b5d02487a97de6
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-10-11 08:19:30 +0530

    Milestone done

--- src/components/Milestones.jsx
+++ src/components/Milestones.jsx
@@ -1,11 +1,107 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import milestone1 from "assets/milestone1.png";
+import milestone2 from "assets/milestone2.png";
+import milestone3 from "assets/milestone3.png";
+import milestoneBackround from "assets/milestone-background.png";
 
-const Milestones = () => {
+
+function Milestones() {
+  const milestone = [
+    {
+      image: milestone1,
+      data: "Cleints Served",
+      amount: "407",
+    },
+    {
+      image: milestone2,
+      data: "of raw data",
+      amount: "10T",
+    },
+    {
+      image: milestone3,
+      data: "recommendation",
+      amount: "235",
+    },
+  ];
   return (
-    <div>
-      Milestones
-    </div>
-  )
+    <Section >
+      <div className="background">
+        <img src={milestoneBackround} alt="Milestone Background" />
+      </div>
+      <div className="milestones">
+        {milestone.map((ms) => {
+          return (
+            <div
+              className="milestone"
+              
+            >
+              <p>{ms.amount}</p>
+              <span>{ms.data}</span>
+              <img src={ms.image} alt="milestone image" />
+            </div>
+          );
+        })}
+      </div>
+    </Section>
+  );
 }
 
-export default Milestones
+const Section = styled.section`
+  height: 100vh;
+  background-color: var(--primary-color);
+  padding: 0 10rem;
+  position: relative;
+  overflow: hidden;
+  .background {
+    position: absolute;
+    left: 0;
+    bottom: -30%;
+    img {
+      z-index: 2;
+      height: 43rem;
+    }
+  }
+  .milestones {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    color: white;
+    align-items: center;
+    height: 100%;
+    .milestone {
+      z-index: 20;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 2rem;
+
+      p {
+        font-size: 5rem;
+        font-weight: bolder;
+        line-height: 3rem;
+      }
+      span {
+        text-transform: uppercase;
+        color: #ffffffc7;
+      }
+      img {
+        height: 6rem;
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    padding: 5rem 2rem;
+    min-height: 100vh;
+    height: 100%;
+    .background {
+      display: none;
+    }
+    .milestones {
+      grid-template-columns: 1fr;
+      gap: 5rem;
+    }
+  }
+`;
+
+export default Milestones;
--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ScrollToTop = () => {
   return (
     <div>
-      ScrollToTop
+      {/* ScrollToTop */}
     </div>
   )
 }
